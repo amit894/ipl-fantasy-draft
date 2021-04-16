25f537796e4648b3713dac0cc76a50f884eeb103
amit894 <amit.894@gmail.com>
2021-04-17 02:01:07 +0530

    Adding Git Ignore

--- src/players.py
+++ src/players.py
@@ -6,23 +6,21 @@ class Players(Matches):
     def __init__(self,teams):
         self.teams=teams
 
-    def matches_per_team(self,team):
-        matches=[]
-        for match in (self.get_all_matches()):
-            if team in match:
-                matches.append(match)
-        return matches
-
-    def matches_per_all_teams(self):
-        matches={}
+    def get_matches_per_teams(self):
+        self.overall_matches={}
         for team in self.teams:
             team_match=[]
-            for match in (self.get_all_matches()):
-                if team in match:
+            for match in self.get_all_matches():
+                if (match.find(team.split('.')[0])>=0):
                     team_match.append(match)
-            matches[team]=team_match
-        return matches
+            self.overall_matches[team]=team_match
+        return self.overall_matches
+
+    def get_stats_all_teams(self):
+        for key in self.overall_matches:
+            print(self.overall_matches[key])
 
 teams = [f for f in listdir("../resources/teams") if isfile(join("../resources/teams", f))]
 P1 = Players(teams)
-print(P1.matches_per_all_teams())
+P1.get_matches_per_teams()
+P1.get_stats_all_teams()
