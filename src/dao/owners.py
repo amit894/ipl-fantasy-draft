1df66c02cc419b54e6db9c95ecd153414742d873
amit894 <amit.894@gmail.com>
2021-04-16 16:14:32 +0530

    Initial checkin

---
+++ src/dao/owners.py
@@ -0,0 +1,22 @@
+import flask
+from flask import request,jsonify
+import json
+
+class Owners():
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+
+    def readPlayers(self):
+        try:
+            f1=open('../resources/owners/'+self.name+'.json','r')
+            players=json.load(f1)
+            f1.close()
+            return players
+        except:
+            return ("Error reading from file")
+            f1.close()
+
+
+O1=Owners("ahuja",1)
+print(O1.readPlayers())
--- src/players.py
+++
@@ -1,7 +0,0 @@
-class Players():
-    def __init__(self,name,id,team):
-        self.name=name
-        self.id=id
-        self.team=team
-
-#    def stats(self)
