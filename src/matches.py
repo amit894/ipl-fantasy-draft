01ca1ae67c7c5b0d9a8774873e6eed58b7f8cc5e
amit894 <amit.894@gmail.com>
2021-04-17 00:53:14 +0530

    Folder Restructure

--- src/matches.py
+++ src/matches.py
@@ -15,7 +15,7 @@ class Matches():
                 teams.append((url["url"].split("/")[5]).split("-")[0])
                 teams.append((url["url"].split("/")[5]).split("-")[2])
                 match_data=get_match(url["url"],teams)
-                status=writeFile("../resources/matches/"+match_id+".json",match_data)
+                status=writeFile("../resources/matches1/"+match_id+"_"+teams[0]+"_"+teams[1]+"_"+".json",match_data)
                 if status=="Successful":
                     url_statuses=readFile("../resources/url.json")
                     for url_status in url_statuses:
@@ -28,3 +28,6 @@ class Matches():
 urls=readFile("../resources/url.json")
 M1=Matches(urls)
 M1.update_match_score()
+
+# x=readFile("../resources/matches/35612.json")
+# print(x[0])
