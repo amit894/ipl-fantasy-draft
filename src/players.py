628fc360efd70c4a726f433610a452c3d6850afb
Amit Raj <amit.raj@walmart.com>
2021-04-17 01:10:55 +0530

    Restrucutre

--- src/players.py
+++ src/players.py
@@ -1,5 +1,18 @@
-class Players():
-    def __init__(self,name,id,team):
-        self.name=name
-        self.id=id
-        self.team=team
+from os import listdir
+from os.path import isfile, join
+from matches import Matches
+
+class Players(Matches):
+    def __init__(self,teams):
+        self.teams=teams
+
+    def matches_per_team(self,team):
+        for match in (P1.get_all_matches()):
+            print(match)
+            if match.find(team) == 0:
+
+
+
+teams = [f for f in listdir("../resources/teams") if isfile(join("../resources/teams", f))]
+P1 = Players(teams)
+P1.matches_per_team("csk")
