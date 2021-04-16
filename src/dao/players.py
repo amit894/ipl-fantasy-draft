ec9f2c6736d7e3145d8d3ccc1975c741122d2e60
Amit Raj <amit.raj@walmart.com>
2021-04-16 20:17:14 +0530

    Adding Funtionalities

--- src/dao/players.py
+++ src/dao/players.py
@@ -1,13 +1,5 @@
-import flask
-from flask import request,jsonify
-import json
-
 class Players():
     def __init__(self,name,id,team):
         self.name=name
         self.id=id
         self.team=team
-
-    @app.route('/all', methods=['GET'])
-    def getAllPlayer(self):
-        return jsonify(readPlayers())
