ce5b9beb3fefd4cb38c9375ce67349dbb9eb60d1
Amit Raj <amit.raj@walmart.com>
2021-04-17 16:49:35 +0530

    Fixing more bugs

--- src/teams.py
+++ src/teams.py
@@ -2,13 +2,15 @@ from os import listdir
 from os.path import isfile, join
 from matches import Matches
 from files import readFile, writeFile, appendFile
+import asyncio
+
 
 
 class Teams(Matches):
     def __init__(self,teams):
         self.team_names=teams
 
-    def update_matches_per_teams(self):
+    async def update_matches_per_teams(self):
         self.overall_matches={}
         for team in self.team_names:
             team_match=[]
@@ -18,22 +20,27 @@ class Teams(Matches):
             self.overall_matches[team]=team_match
         return self.overall_matches
 
-    def get_teams(self):
+    async def get_teams(self):
         return(self.team_names)
 
-    def update_stats_all_teams(self):
+    async def update_stats_all_teams(self):
         for key in self.overall_matches:
-            overall_team_data={}
-            for match_file in self.overall_matches[key]:
-                match_data=readFile("../resources/matches/"+match_file)
-                team_data=[]
-                for key1 in match_data:
-                    if (key1.find(key.split('.')[0])>=0):
-                        team_data.append(match_data[key1])
-                overall_team_data[match_file.split('-')[0]]=team_data
-            writeFile("../resources/teams/"+key, overall_team_data)
+            if (key=="dc.json"):
+                overall_team_data={}
+                for match_file in self.overall_matches[key]:
+                    match_data=readFile("../resources/matches/"+match_file)
+                    for key1 in match_data:
+                        team_data=[]
+                        if ((key.split('.')[0])==(key1.split('_')[0])):
+                            team_data.append(match_data[key1])
+                            print(team_data)
+                        overall_team_data[match_file.split('-')[0]]=team_data
+                    #writeFile("../resources/teams/"+key, overall_team_data)
 
-teams = [f for f in listdir("../resources/teams") if isfile(join("../resources/teams", f))]
-T1 = Teams(teams)
-T1.update_matches_per_teams()
-T1.update_stats_all_teams()
+async def main():
+    teams = [f for f in listdir("../resources/teams") if isfile(join("../resources/teams", f))]
+    T1 = Teams(teams)
+    await T1.update_matches_per_teams()
+    await T1.update_stats_all_teams()
+
+asyncio.run(main())
