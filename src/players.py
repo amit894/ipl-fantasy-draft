7b177adc6c59fc8cbea985e4fa860aec43f92c12
Amit Raj <amit.raj@walmart.com>
2021-04-17 15:06:31 +0530

    Making changes to players file

--- src/players.py
+++ src/players.py
@@ -2,19 +2,32 @@ from os import listdir
 from os.path import isfile, join
 from teams import Teams
 from files import readFile, writeFile, appendFile
+import json
 
 class Players(Teams):
     def __init__(self,teams):
         super().__init__(teams)
 
     def get_stats(self):
+        player_stats={}
+
         for teams in self.get_teams():
             team=readFile("../resources/teams/"+teams)
             for match in team:
+                match_stat={}
                 for innings in team[match]:
-                    for player in innings:
-                        print(jsonify(innings[player]))
-                        print("De Limiter")
+                    for inning in innings:
+                        for player in inning:
+                            if isinstance(player,dict):
+                                player_name=player["name"]
+                                stat={}
+                                if 'wickets' in player:
+                                    stat["bowling_stat"]=player
+                                if 'runs' in player:
+                                    stat["batting_stat"]=player
+                        match_stat[match]=stat
+                        player_stats[player_name]=match_stat
+        print(player_stats)
 
 teams = [f for f in listdir("../resources/teams") if isfile(join("../resources/teams", f))]
 P1=Players(teams)
