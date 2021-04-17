503877493ef38f6ded50fd8bafcc85b4cf5842d9
Amit Raj <amit.raj@walmart.com>
2021-04-17 15:51:18 +0530

    Some more bug fixes

--- src/players.py
+++ src/players.py
@@ -9,29 +9,33 @@ class Players(Teams):
         super().__init__(teams)
 
     def get_stats(self):
-        player_stats={}
+        self.player_stats={}
 
         for teams in self.get_teams():
             team=readFile("../resources/teams/"+teams)
-            match_stat={}
             for match in team:
                 for innings in team[match]:
                     for inning in innings:
                         for player in inning:
+                            match_stat={}
+                            player_name=''
+                            stat={}
                             if isinstance(player,dict):
-                                player_name=player["name"]
-                                stat={}
+                                player_name=player["name"]+"_"+teams.split(".")[0]
                                 if 'wickets' in player:
                                     stat["bowling_stat"]=player
                                 if 'runs' in player:
                                     stat["batting_stat"]=player
                             else:
                                 for x in inning:
-                                    player_name=x
+                                    player_name=x+"_"+teams.split(".")[0]
                                     stat["fielding_stat"]=inning[x]
-                        match_stat[match]=stat
-                player_stats[player_name]=match_stat
-        print(player_stats)
+                            match_stat[match]=stat
+                            self.player_stats[player_name]=match_stat
+        print(self.player_stats)
+
+    def update_stats(self):
+        print(self.get_teams())
 
 teams = [f for f in listdir("../resources/teams") if isfile(join("../resources/teams", f))]
 P1=Players(teams)
