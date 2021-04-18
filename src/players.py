3ace2571560decba53038332a0430b27ce03c3ca
amit894 <amit.894@gmail.com>
2021-04-18 17:03:51 +0530

    Fixing Duplicate Players Issue

--- src/players.py
+++ src/players.py
@@ -11,7 +11,7 @@ class Players(Teams):
 
     async def get_stats(self):
         player_stats=[]
-        temp_list=[]
+        player_list=[]
         players={"test_player"}
 
         for teams in self.get_teams():
@@ -40,22 +40,22 @@ class Players(Teams):
                                         stat[player_name[0]+"_"+str(match)+"_fielding_stat"]=player[key]
                                         player_stats.append(stat)
                                         players.add(player_name[0])
-        temp_list.append(players)
-        temp_list.append(player_stats)
-        return temp_list
+
+        player_list.append(players)
+        player_list.append(player_stats)
+        return player_list
 
 
     async def update_player_stats(self,players,player_stats):
         updated_player_stats={}
         sorted_players=list(players)
         sorted_players.sort()
-        #print(sorted_players))
         for key1 in sorted_players:
             l1=[]
             for key2 in player_stats:
                 for key3 in key2:
                     temp_dict={}
-                    if(key1.split("_")[0].find(key3.split("_")[0])==0):
+                    if(key1.split("_")[0].find(key3.split("_")[0])>=0):
                         temp_dict[key3.split("_")[2]+"_"+key3.split("_")[3]]=key2[key3]
                         l1.append(temp_dict)
             updated_player_stats[key1]=l1
@@ -64,45 +64,44 @@ class Players(Teams):
         return(updated_player_stats)
 
 
-    # async def update_players(self):
-    #     #temp_players={"Rajat Patidar_rcb":0, "Patidar_rcb":0, "Amit Raj":0, "Raj":0}
-    #
-    #     temp_players={}
-    #     for player in self.players:
-    #         temp_players[player]=0
-    #
-    #
-    #     for key1 in temp_players:
-    #         for key2 in temp_players:
-    #             if(key2.find(key1)>0 and temp_players[key2]==0):
-    #                 temp_players[key2]=1
-    #     #print(temp_players)
-    #
-    #     l1=[]
-    #     l2=[]
-    #
-    #     for key in temp_players:
-    #        if (temp_players[key]==0):
-    #            l1.append(key)
-    #        else:
-    #            l2.append(key)
-    #
-    #     print(len(l1),len(l2),len(temp_players))
+    async def diff(self,li1,li2):
+            return (list(list(set(li1)-set(li2)) + list(set(li2)-set(li1))))
+
+    async def twoStrings(s1, s2):
+        v = [0] * (MAX_CHAR)
+        for i in range(len(s1)):
+            v[ord(s1[i]) - ord('a')] = True
+        for i in range(len(s2)) :
+            if (v[ord(s2[i]) - ord('a')]) :
+                return True
+
+        return False
+
+
+    async def remove_duplicate_elements(self,list1):
+        masterlist=readFile("../resources/players.json")
+        temp_list=set()
+        for player in list1:
+            temp_list.add(masterlist[player.split("_")[0]]+'_'+player.split("_")[1])
+        print(len(list1),len(temp_list))
+        list1=list(temp_list)
+        return(list1)
+
+
 
     async def update_stats(self,updated_player_stats):
         overall_stats=updated_player_stats
-        print(len(updated_player_stats))
         for players in updated_player_stats:
-            # print(updated_player_stats[players])
-            # print("delimiters")
             writeFile("../resources/player_scores/"+players.split('_')[1]+"/"+players.split('_')[0],overall_stats[players])
 
 
 async def main():
     teams = [f for f in listdir("../resources/team_scores") if isfile(join("../resources/team_scores", f))]
     P1=Players(teams)
-    player_stats=await P1.get_stats()
-    updated_player_stats=await P1.update_player_stats(player_stats[0],player_stats[1])
+    main_player_stats=await P1.get_stats()
+    main_player_stats[0]=await P1.remove_duplicate_elements(main_player_stats[0])
+    # #print(main_player_stats[0])
+    updated_player_stats=await P1.update_player_stats(main_player_stats[0],main_player_stats[1])
     await P1.update_stats(updated_player_stats)
 
 asyncio.run(main())
