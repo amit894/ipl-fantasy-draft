d6f9fd3703265dced77dcb1eef6f5a377e4745ef
amit894 <amit.894@gmail.com>
2021-04-19 10:50:35 +0530

    Best 11 points

--- src/owners.py
+++ src/owners.py
@@ -17,6 +17,12 @@ class Owners():
             print(e)
             return ("Error reading from file")
 
+    def sum11(self,list):
+        sum=0
+        for i in range(11):
+            sum+=list[i]
+        return sum
+
 
 owners=["ahuja","bapu","supan","shreyans","dusty","pranky","bha1"]
 
@@ -25,17 +31,22 @@ for i in range(len(owners)):
     temp_dict={}
     O1=Owners(owners[i],i)
     players=O1.readPlayers()
+    player_list=[]
+    player_sum=0
     for player in players:
         #print(player['player'])
+        innings_sum=0
         json_data=readFile("../resources/points/players/"+player['player'])
         if ("Error" in json_data):
             owner_sum+=0
         else:
-            player_sum=0
             for data in json_data:
-                #print(json_data[data])
-                player_sum+=int(json_data[data])
-            owner_sum+=player_sum
-    print(owner_sum)
+                innings_sum+=int(json_data[data])
+            player_sum+=innings_sum
+        player_list.append(innings_sum)
+        player_list.sort(reverse=True)
+    owner_sum=O1.sum11(player_list)
+    #print(player_list)
     temp_dict[owners[i]]=owner_sum
+    print(owner_sum)
     appendFile("../resources/points/owners/points.json", temp_dict)
